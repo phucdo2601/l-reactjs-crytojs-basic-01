e145db148e9ea0a7bdcb2ce71e8688f6ebe96d1a
phucdo2601 <ngocphucdo2601@gmail.com>
2022-06-23 08:34:54 +0700

    First learn cryto-js with reactjs encrypt and decryted data b01

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+var CryptoJS = require("crypto-js");
 
 function App() {
+  let data = [
+    {
+      id: 1,
+      name: "Phuc",
+    },
+    {
+      id: 2,
+      name: "Loan",
+    },
+  ];
+
+  /**
+   * Encrypt
+   */
+  var ciphertext = CryptoJS.AES.encrypt(
+    JSON.stringify(data),
+    "my-secret-key@123"
+  ).toString();
+  //log encrypted data
+  console.log("Encrypt Data -");
+  console.log(ciphertext);
+
+  /**
+   * Decrypt
+   */
+
+  var byte = CryptoJS.AES.decrypt(ciphertext, "my-secret-key@123");
+  var decryptedData = JSON.parse(byte.toString(CryptoJS.enc.Utf8));
+
+  //log decrypted Data
+  console.log("decrypted Data -");
+  console.log(decryptedData);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <>
+          <div>Encrypt data - {ciphertext}</div>
+          <div>
+            {decryptedData.map((obj) => (
+              <>
+                <div className="">{obj.id}</div>
+                <div className="">{obj.name}</div>
+              </>
+            ))}
+          </div>
+        </>
       </header>
     </div>
   );
