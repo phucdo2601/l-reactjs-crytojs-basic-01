aaecb79304ccbc72e2d83493e3748a9f290b4cf5
phucdo2601 <ngocphucdo2601@gmail.com>
2022-06-23 09:07:46 +0700

    First learn basic crypto-js with react + ts encrypt and descrypt data b01

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,65 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var CryptoJS = require('crypto-js');
+
+interface dataObj {
+  id: number;
+  name?: string | any;
+}
+
 function App() {
+
+  var data: Array<dataObj> = [{ id: 1, name: 'PhucDn' }, { id: 2, name: 'LanTran' }]
+
+  /**
+   * ma hoa va giai ma dua tren cai key my-secret-key@123, CHUOI NAY PHAI DC BAO MAT KHONG DE LO
+   */
+
+  // Encrypt
+  var ciphertext = CryptoJS.AES.encrypt(JSON.stringify(data), 'my-secret-key@123').toString();
+  //log encrypted data
+  console.log('Encrypt Data -')
+  console.log(ciphertext);
+
+  // Decrypt
+  var bytes = CryptoJS.AES.decrypt(ciphertext, 'my-secret-key@123');
+  var decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+
+  //log decrypted Data
+  console.log('decrypted Data -')
+  console.log(decryptedData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <div>Case data:</div>
+          <div>
+            {
+              data.map((d) => (
+                <>
+                  <div>{d.id}</div>
+                  <div>{d.name}</div>
+                </>
+              ))
+            }
+          </div>
+          <div>--------------------------------------------------------------------------------------</div>
+          <div>Encrypt Data -{ciphertext}</div>
+          <div>-------------------------------------------------------------------------------------</div>
+          <div>Decrypt Data: </div>
+          <div>
+            {decryptedData.map((obj: dataObj) => (
+              <>
+                <div>{obj.id}</div>
+                <div>{obj.name}</div>
+              </>
+            ))}
+          </div>
+        </header>
+      </div>
+    </>
   );
 }
 
